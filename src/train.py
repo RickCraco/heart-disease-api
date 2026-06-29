0f4afc3a98f4a852b76748e3dbcb00ae719a9ea8
Riccardo <riccardocracolici43@gmail.com>
2026-06-29 15:07:46 +0200

    created file for training + added core imports

---
+++ src/train.py
@@ -0,0 +1,20 @@
+# dataset loading
+from src.utils import download_dataset
+# data handling
+import pandas as pd
+import numpy as np
+# preprocessing
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+# metrics
+from sklearn.metrics import classification_report
+from sklearn.metrics import roc_auc_score
+# training
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+# model
+import xgboost
+# logging
+from src.logger import setup_logger
