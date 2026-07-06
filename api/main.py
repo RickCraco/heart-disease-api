d7b707e7aa43568b3c628fa0e97418320bbbe1bd
Riccardo <riccardocracolici43@gmail.com>
2026-07-06 12:53:50 +0200

    Initialize FastAPI application and load XGBoost model from specified path in main.py.

--- api/main.py
+++ api/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
 import joblib
+import pathlib
 import pandas as pd
 from api.schemas import PatientData, PredictionResponse
 
+app = FastAPI()
+
+MODEL_PATH = pathlib.Path("models/xgboost_pipeline.joblib")
+model = joblib.load(MODEL_PATH)
